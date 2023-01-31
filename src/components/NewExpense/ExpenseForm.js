a3baf73a4879e3f5b45a9caa1fff8ce218876a2e
Alex <30435251+AlPeFe@users.noreply.github.com>
2023-01-31 09:20:38 +0100

    s

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,25 +1,79 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+
+    const[enteredTitle, setEnteredTitle] =  useState('');
+    const[enteredAmount, setEnteredAmount] = useState('');
+    const[enteredDate, setEnteredDate] = useState('');
+
+    //u can also do it with only 1 useState
+    // const[userInput, setUserInput] = useState({
+    //     enteredTitle:'',
+    //     enteredAmount: '',
+    //     enteredDate:''
+    // })
+
+    const titleChangleHandler = (event) =>{
+        setEnteredTitle(event.target.value);
+
+        //esta es la forma de hacerlo cuando solo hay 1 state para todos los cambios, se pueden usar ambos el individual o el multiple pero recomienda multimple
+        // setUserInput((prevState)=>{ 
+        //     return {
+        //         ...prevState,
+        //         enteredTitle: event.target.value
+        //     }
+        // });
+    };
+
+    const amountChangedHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    };
+
+    const dateChangedHandler = (event)=>{
+        setEnteredDate(event.target.value);
+    };
+
+    const submitHandler =(e) => {
+        e.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        console.log(expenseData);
+        
+        setEnteredAmount('');
+        setEnteredDate('');
+        setEnteredTitle('');
+    }
+
     return(
-    <form>
+    <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text"/>
+                <input value={enteredTitle} type="text" onChange={titleChangleHandler}/>
             </div>
         </div>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" />
+                <input value={enteredAmount} type="number" min="0.01" step="0.01" onChange={amountChangedHandler} />
             </div>
         </div>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31"/>
+                <input 
+                    value={enteredDate}
+                    type="date" 
+                    min="2019-01-01" 
+                    max="2022-12-31"
+                    onChange={dateChangedHandler}
+                />
             </div>
         </div>
         <div className="new-expense__actions">
